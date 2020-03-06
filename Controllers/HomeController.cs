be5a1f4644c156dc458045e97de6f5e44b858e35
unknown <holmanryan4@gmail.com>
2020-03-06 10:55:57 -0600

    Iam working on the webpage setting up the layout

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -27,6 +27,14 @@ namespace Authentication.Controllers
         {
             return View();
         }
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
